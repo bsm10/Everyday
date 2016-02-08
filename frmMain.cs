6097f6a8cf3417773fffbfaa989225b9438f3939
bsm10 <bsm10@i.ua>
2016-02-08 20:35:07 +0200

    Img

--- frmMain.cs
+++ frmMain.cs
@@ -1,16 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
-using System.Net;
-using System.IO;
 //using System.Runtime.Serialization.Json;
-using System.Runtime.Serialization;
-using Newtonsoft.Json;
 //using System.Web;
 
 namespace Everyday
@@ -19,9 +10,14 @@ namespace Everyday
 
     public partial class frmMain : Form
     {
-        public frmMain()
+        public frmMain(Everyday everyday)
         {
             InitializeComponent();
+
+            pbxKlient.Image = everyday.UserImg;
+
+            
+
         }
 
     }
